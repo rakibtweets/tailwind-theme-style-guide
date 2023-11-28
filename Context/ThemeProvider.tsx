795e7f674f5f46b53ab19231311069b69cdba19f
Rakib Hasan <rakibhasanroky0@gmail.com>
2023-11-28 22:32:50 +0600

    Update ESLint and Tailwind configuration

---
+++ Context/ThemeProvider.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import React, { createContext, useState, useEffect } from 'react';
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState('');
+
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      setMode('dark');
+      document.documentElement.classList.add('dark');
+    } else {
+      setMode('light');
+      document.documentElement.classList.remove('dark');
+    }
+  };
+
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
+
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
---
+++ hooks/useTheme.tsx
@@ -0,0 +1,12 @@
+import { ThemeContext } from '@/Context/ThemeProvider';
+import { useContext } from 'react';
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+
+  if (context === undefined) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return context;
+}
